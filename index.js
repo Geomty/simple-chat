a6a568a8d132c4e9e18c03ba650f7c919854d3a5
Geomty <69478549+Geomty@users.noreply.github.com>
2024-08-13 17:03:00 -0400

    test

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { WebSocketServer } = require("ws");
 
+const wss = new WebSocketServer({ port: 8080 });
+let array = [];
+
+wss.on("listening", () => {
+    console.log("Websocket server is online");
+});
+
+wss.on("connection", ws => {
+    array.push(ws);
+    ws.on("message", data => {
+        for (const i of array) {
+            i.send(data.toString());
+        }
+    });
+});
